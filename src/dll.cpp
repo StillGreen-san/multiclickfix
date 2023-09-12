ce3b8f4c3f9fc3a4a04b3cc2d651bdc245f0e802
StillGreen-san <40620628+stillgreen-san@users.noreply.github.com>
2023-09-12 09:31:04 +0200

    move button logic into struct

--- src/dll.cpp
+++ src/dll.cpp
@@ -7,10 +7,38 @@
 constexpr LRESULT STOP_HOOK_CHAIN_RESULT = 1;
 constexpr DWORD MIN_TIME_DELTA = 24;
 
+struct Button
+{
+	bool handleDown(DWORD clickTime)
+	{
+		if(clickTime - lastClickTime < MIN_TIME_DELTA)
+		{
+			downSkipped = true;
+			return true;
+		}
+		return false;
+	}
+	bool handleUp(DWORD clickTime)
+	{
+		if(downSkipped)
+		{
+			downSkipped = false;
+			return true;
+		}
+		if(clickTime - lastClickTime < MIN_TIME_DELTA)
+		{
+			return true;
+		}
+		lastClickTime = clickTime;
+		return false;
+	}
+	bool downSkipped = false;
+	DWORD lastClickTime = 0;
+};
+
 HHOOK hkKey = nullptr;
 HINSTANCE hInstHookDll = nullptr;
-bool leftButtonDownSkipped = false;
-DWORD lastClickTime = 0;
+Button leftClick;
 
 BOOL /*APIENTRY*/ DllMain(HANDLE hModule, DWORD reasonForCall, LPVOID lpReserved)
 {
@@ -29,37 +57,29 @@ LRESULT /*CALLBACK*/ LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
 		return CallNextHookEx(hkKey, nCode, wParam, lParam);
 	}
-	MSLLHOOKSTRUCT& msllhookstruct = *reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
+	const MSLLHOOKSTRUCT& msllhookstruct = *reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
 	if(msllhookstruct.flags & LLMHF_INJECTED)
 	{
 		return CallNextHookEx(hkKey, nCode, wParam, lParam);
 	}
-	const DWORD timeDelta = msllhookstruct.time - lastClickTime;
 	switch(wParam)
 	{
 	case WM_LBUTTONDOWN:
-		if(timeDelta < MIN_TIME_DELTA)
+		if(leftClick.handleDown(msllhookstruct.time))
 		{
-			leftButtonDownSkipped = true;
 			return STOP_HOOK_CHAIN_RESULT;
 		}
 		break;
 	case WM_LBUTTONUP:
-		if(leftButtonDownSkipped)
-		{
-			leftButtonDownSkipped = false;
-			return STOP_HOOK_CHAIN_RESULT;
-		}
-		if(timeDelta < MIN_TIME_DELTA)
+		if(leftClick.handleUp(msllhookstruct.time))
 		{
 			return STOP_HOOK_CHAIN_RESULT;
 		}
-		lastClickTime = msllhookstruct.time;
 		break;
-//	case WM_RBUTTONDOWN:
-//	case WM_RBUTTONUP:
-//	case WM_MOUSEMOVE:
-//	case WM_MOUSEWHEEL:
+		//	case WM_RBUTTONDOWN:
+		//	case WM_RBUTTONUP:
+		//	case WM_MOUSEMOVE:
+		//	case WM_MOUSEWHEEL:
 	}
 	return CallNextHookEx(hkKey, nCode, wParam, lParam);
 }
